ef07b7d6d0a8704e00a32524513620d33ada5ff7
waynekf <misterdotnet@hotmail.com>
2025-08-06 22:38:00 +0100

    Add CORS headers

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://your-frontend.com"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Private-Network", true);
+  res.setHeader("Access-Control-Max-Age", 7200);
+
+  next();
+});
+
 const {
   getBranches,
   getBranch,
