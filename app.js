c3e7fe447e28a711d21009f41b24008c9f1718c2
waynekf <misterdotnet@hotmail.com>
2025-08-07 08:31:16 +0100

    Update CORS

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://your-frontend.com"
+    "*"
   );
   res.setHeader(
     "Access-Control-Allow-Methods",
